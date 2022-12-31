3ab423ca739b58a8e1a59353d7d3202879b471b9
Daniel Acuna <4857535+moustacheful@users.noreply.github.com>
2022-12-30 20:10:23 -0500

    refactor a bit

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,55 @@
 use futures::{future::select_all, StreamExt};
-use std::{sync::Arc, time::Duration};
+use std::time::Duration;
 
+#[derive(Debug)]
 struct Pipeline {
     name: String,
-    pub tx: flume::Sender<u16>,
-    pub rx: flume::Receiver<u16>,
+    pub tx: flume::Sender<u64>,
+    pub rx: flume::Receiver<u64>,
 }
 
 impl Pipeline {
     pub fn new(name: String) -> Pipeline {
-        let (tx, rx) = flume::unbounded::<u16>();
+        let (tx, rx) = flume::unbounded::<u64>();
 
         Pipeline { tx, rx, name }
     }
 
-    pub fn send(&self, item: u16) {
-        dbg!("SENDING THE THING", &item);
-        self.tx.send(item).unwrap();
-    }
-
     pub async fn listen(self) {
         let name = self.name.clone();
-        let rx = self.rx.clone();
+        let mut stream = self.rx.into_stream().map(|v| v + 10);
 
-        while let Ok(item) = rx.recv_async().await {
+        println!("{:?} listening", &name);
+
+        while let Some(item) = stream.next().await {
             println!("{:?} - by {:?}", item, &name);
         }
     }
 }
 
 struct App {
-    pub tx: flume::Sender<u16>,
-    pub rx: flume::Receiver<u16>,
-    pub pipelines: Arc<Vec<Pipeline>>,
+    pub tx: flume::Sender<u64>,
+    pub rx: flume::Receiver<u64>,
+    pub pipelines: Vec<Pipeline>,
 }
 
 impl App {
-    pub fn new(
-        tx: flume::Sender<u16>,
-        rx: flume::Receiver<u16>,
-        pipelines: Arc<Vec<Pipeline>>,
-    ) -> App {
+    pub fn new(tx: flume::Sender<u64>, rx: flume::Receiver<u64>, pipelines: Vec<Pipeline>) -> App {
         App { tx, rx, pipelines }
     }
 
     pub async fn run_broadcast(self) -> Option<()> {
         let pipelines = self.pipelines;
-        let txs = pipelines.iter().map(|p| p.tx.clone());
+        let txs = pipelines.iter().map(|p| p.tx.clone()).collect::<Vec<_>>();
         let rx = self.rx.clone();
 
-        let futs = pipelines.iter().map(|p| {
-            tokio::spawn(async {
-                println!("{:?} listening", p.name);
-                Box::pin(p.listen()).await
-            })
-        });
+        let futs = pipelines
+            .into_iter()
+            .map(|p| tokio::spawn(async { Box::pin(p.listen()).await }))
+            .collect::<Vec<_>>();
 
         while let Ok(item) = rx.recv_async().await {
-            for tx in txs {
+            for tx in &txs {
                 tx.send(item.clone()).unwrap();
             }
         }
@@ -68,9 +60,9 @@ impl App {
     }
 }
 
-#[tokio::main(flavor = "multi_thread")]
+#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
-    let (tx, mut rx) = flume::unbounded::<u16>();
+    let (tx, rx) = flume::unbounded::<u64>();
 
     let app_tx = tx.clone();
     let app_rx = rx.clone();
@@ -79,10 +71,10 @@ async fn main() {
         let app = App::new(
             app_tx,
             app_rx,
-            Arc::new(vec![
-                Pipeline::new("First".to_string()),
-                Pipeline::new("Last".to_string()),
-            ]),
+            vec![
+                Pipeline::new("One".to_string()),
+                Pipeline::new("Two".to_string()),
+            ],
         );
 
         app.run_broadcast().await;
@@ -92,12 +84,20 @@ async fn main() {
 
     tokio::time::sleep(Duration::from_secs(1)).await;
 
+    println!("Sending messages");
     tx.send(10).unwrap();
     tx.send(20).unwrap();
     tx.send(30).unwrap();
     tx.send(40).unwrap();
     tx.send(50).unwrap();
     tx.send(60).unwrap();
+
+    println!("huh");
+
+    let mut s = tokio::time::interval(Duration::from_millis(1000));
+    while let now = s.tick().await {
+        tx.send(now.elapsed().as_secs()).unwrap();
+    }
 
     f.await;
 }
