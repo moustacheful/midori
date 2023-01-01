59416220336b3281d4f51840cb67b1d60de1bccf
Daniel Acuna <4857535+moustacheful@users.noreply.github.com>
2023-01-01 00:31:36 -0500

    works for me!

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,104 @@
-use futures::{future::select_all, StreamExt};
-use std::time::Duration;
+use futures::{future::select_all, Stream, StreamExt};
+use std::{pin::Pin, time::Duration};
+use tokio_stream::wrappers::IntervalStream;
+
+struct Tempo {
+    main: IntervalStream,
+    beat_interval: u64,
+}
+
+impl Tempo {
+    fn get_interval_stream(interval_ms: u64) -> IntervalStream {
+        println!("beginning tempo with {}", interval_ms);
+        let interval = tokio::time::interval(Duration::from_millis(interval_ms));
+
+        tokio_stream::wrappers::IntervalStream::new(interval)
+    }
+
+    pub fn new(bpm: u64) -> Tempo {
+        let beat_interval = ((bpm as f64 / 70.0) * 1000.0) as u64;
+        let main = Self::get_interval_stream(beat_interval);
+
+        Tempo {
+            main,
+            beat_interval,
+        }
+    }
+
+    pub async fn subdiv(&mut self, factor: u64) -> IntervalStream {
+        self.main.next().await;
+
+        Self::get_interval_stream(self.beat_interval / factor)
+    }
+}
+
+#[derive(Debug)]
+struct Transform {}
+
+impl Transform {
+    pub fn on_tempo() {}
+
+    pub fn on_message() {}
+}
+
+impl Stream for Transform {
+    type Item = u64;
+
+    fn poll_next(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        std::task::Poll::Pending
+    }
+}
 
 #[derive(Debug)]
 struct Pipeline {
     name: String,
     pub tx: flume::Sender<u64>,
     pub rx: flume::Receiver<u64>,
+    pub transforms: Vec<Transform>,
 }
 
+#[derive(Debug)]
+enum Wrapper<T> {
+    Tempo,
+    Value(T),
+}
 impl Pipeline {
     pub fn new(name: String) -> Pipeline {
         let (tx, rx) = flume::unbounded::<u64>();
 
-        Pipeline { tx, rx, name }
+        Pipeline {
+            tx,
+            rx,
+            name,
+            transforms: vec![Transform {}, Transform {}],
+        }
     }
 
     pub async fn listen(self) {
         let name = self.name.clone();
-        let mut stream = self.rx.into_stream().map(|v| v + 10);
+        let mut tempo = Tempo::new(60);
+
+        let streams: Vec<Pin<Box<dyn Stream<Item = Wrapper<u64>> + Send>>> = vec![
+            Box::pin(self.rx.into_stream().map(|val| Wrapper::Value(val))),
+            Box::pin(tempo.subdiv(1).await.map(|instant| Wrapper::Value(1))),
+            Box::pin(tempo.subdiv(2).await.map(|instant| Wrapper::Value(2))),
+            Box::pin(tempo.subdiv(8).await.map(|instant| Wrapper::Value(8))),
+        ];
+
+        let mut stream = futures::stream::select_all::select_all(streams);
+
+        let transforms = self.transforms;
 
         println!("{:?} listening", &name);
 
         while let Some(item) = stream.next().await {
+            transforms.iter().for_each(|t| {
+                // dbg!(t);
+            });
+
             println!("{:?} - by {:?}", item, &name);
         }
     }
@@ -82,8 +159,6 @@ async fn main() {
         Some(())
     });
 
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
     println!("Sending messages");
     tx.send(10).unwrap();
     tx.send(20).unwrap();
@@ -92,12 +167,5 @@ async fn main() {
     tx.send(50).unwrap();
     tx.send(60).unwrap();
 
-    println!("huh");
-
-    let mut s = tokio::time::interval(Duration::from_millis(1000));
-    while let now = s.tick().await {
-        tx.send(now.elapsed().as_secs()).unwrap();
-    }
-
-    f.await;
+    f.await.unwrap();
 }
