118f7bce60cb799b3d55d08c1ffa0cbe45685312
Daniel Acuna <4857535+moustacheful@users.noreply.github.com>
2023-02-20 17:25:49 -0500

    distribute: track noteons and emit noteoffs

--- src/transforms/distribute_transform.rs
+++ src/transforms/distribute_transform.rs
@@ -1,7 +1,7 @@
 use super::Transform;
 use crate::{
     iter_utils::{Cycle, CycleDirection},
-    midi_event::{MIDIEvent, MIDIRouterEvent},
+    midi_event::{MIDIEvent, MIDIRouterEvent, NoteEvent, Wrap},
     scheduler::SchedulerHandler,
 };
 use serde::Deserialize;
@@ -12,6 +12,7 @@ pub struct DistributeTransformOptions {
 }
 
 pub struct DistributeTransform {
+    pressed_keys: Vec<NoteEvent>,
     between_iter: Cycle<u8>,
 }
 
@@ -19,6 +20,7 @@ impl DistributeTransform {
     pub fn from_config(config: DistributeTransformOptions) -> Self {
         Self {
             between_iter: Cycle::new(config.between, CycleDirection::Forward, None),
+            pressed_keys: vec![],
         }
     }
 }
@@ -27,13 +29,30 @@ impl Transform for DistributeTransform {
     fn on_message(
         &mut self,
         mut message: MIDIRouterEvent,
-        _scheduler: &SchedulerHandler,
+        scheduler: &SchedulerHandler,
     ) -> Option<MIDIRouterEvent> {
         match message.event {
             MIDIEvent::NoteOn(ref mut note) => {
                 note.channel = *self.between_iter.next();
 
+                self.pressed_keys.push(note.clone());
+
                 Some(message)
+            }
+            MIDIEvent::NoteOff(NoteEvent { note, .. }) => {
+                // Remove all keys with this note
+                self.pressed_keys.retain(|n| {
+                    let should_keep = n.note != note;
+
+                    // If this is to be removed, send a note off immediately
+                    if !should_keep {
+                        scheduler.send_now(n.get_note_off().wrap())
+                    }
+
+                    should_keep
+                });
+
+                None
             }
             _ => None,
         }
